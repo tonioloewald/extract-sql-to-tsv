ce1390602cfcdda86f96493554d602f7ce34dbfd
Tonio Loewald <tonio@loewald.com>
2023-03-29 17:48:49 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # extract-sql-to-tsv
- Working with SQL dumps is super annoying. This script pulls all the tables out and saves them as TSV.
+
+Working with SQL dumps is super annoying.
+
+## Usage
+
+    node extract.js foo.sql
+
+## What it does
+
+1. finds every `INSERT INTO` statement (assumes one per database row). Tries to handle
+   multiline statements (i.e. with free text in them).
+2. saves each statement in a list attached to its particular table
+3. saves out a file named `<table-name>.tsv` for each table
+
+> Why TSV? Because CSV (and SQL) are incredibly stupid ways to save columnar data.
+> Because you can include quotation marks in strings, CSV and SQL need stateful 
+> parsers and have all kinds of awful edge cases, whereas TSV can be parsed with a 
+> single line of code.
+
+It has been tested on exactly one postgres file, but this file did include a lot of 
+user-generated text with idiotic  stuff like `''` being typed in text fields instead 
+of `"`.
+
+I'm making this available publicly for free just in face someone else finds it helpful.
+And I'm putting it in github so I never have to do it again.
+
+Cheers!
---
+++ extract.js
@@ -0,0 +1,125 @@
+// extracts tables from sql dump as TSV
+// node extract.js foo.sql
+
+const fs = require('fs');
+
+function fatalError(...args) {
+  console.error(...args)
+  process.exit()
+}
+
+let [,, fileName] = process.argv
+
+function getCells(sqlString) {
+  const cells = []
+  const origString = sqlString
+  // attempt to fix bizarre JSON encoding
+  sqlString = sqlString.replace(/''([^':]+)'':/g, '"$1":')
+                       .replace(/: ''([^':]*)''/g, ': "$1"')
+  while (sqlString) {
+    if (sqlString.startsWith('\'\', ') || sqlString === '\'\'') {
+      cells.push('')
+      sqlString = sqlString.substring(4)
+    } else if (sqlString.startsWith('\'')) {
+      let endIndex
+      try {
+        endIndex = sqlString.match(/([^']|'')'(,|$)/).index
+      } catch(e) {
+        console.error('cell termination error', e, sqlString, origString)
+        process.exit()
+      }
+      cells.push(sqlString.substring(1, endIndex + 1))
+      sqlString = sqlString.substring(endIndex + 4)
+    } else {
+      const endIndex = sqlString.indexOf(',')
+      cells.push(sqlString.substring(0, endIndex))
+      sqlString = endIndex > -1 ? sqlString.substring(endIndex + 2) : ''
+    }
+  }
+  return cells
+}
+
+fs.readFile(fileName, 'utf8', (err, source) => {
+  if (err) {
+    fatalError(err)
+    return
+  }
+  
+  const inserts = []
+  const lines = source.split('\n')
+  
+  const tables = {}
+  
+  for(let i = 0; i < lines.length; i++) {
+    let line = lines[i]
+    if (line && line.startsWith('INSERT INTO')) {
+      const tableName = line.split(' ')[2]
+      const record = [line]
+      while(line != null && !line.match(/(, \w+|')\);$/)) {
+        i += 1
+        line = lines[i]
+        record.push(line)
+      }
+      if (!tables[tableName]) {
+        tables[tableName] = {
+          inserts: []
+        }
+      }
+      tables[tableName].inserts.push(record.join('\\n'))
+      /*
+      if (record[0].startsWith(`INSERT INTO ${prefix}${tableName} `)) {
+        inserts.push(record.join('\\n'))
+      }
+      */
+    }
+  }
+  
+  Object.keys(tables).forEach(tableName => {
+    const table = tables[tableName]
+    const {inserts} = table
+    const columns = inserts[0].match(/\(([^)]+)\) VALUES/)[1].split(', ').map(x => x.startsWith('"') ? x.substring(1, x.length - 2) : x)
+    const rows = [columns]
+    
+    inserts.forEach(insert => {
+      let cellData, cells
+      try {
+        try {
+          const startIndex = insert.indexOf(' VALUES (')
+          if (startIndex > -1)
+          cellData = insert.substring(startIndex + 9, insert.length -2)
+        } catch(e) {
+          console.log('cellData error', e, insert)
+          process.exit()
+        }
+        cells = getCells(cellData)
+        if (cells.length !== columns.length) {
+          const obj = {}
+          columns.forEach((key, idx) => obj[key] = cells[idx])
+          console.log({cellData, cells, columns, obj})
+          console.error(cells, 'has wrong number of values, found', cells.length, 'expected', columns.length)
+          process.exit()
+        }
+        rows.push(cells)
+      } catch(e) {
+        console.error('error:', e)
+        console.log({insert, cellData, cells})
+        process.exit()
+      }
+    })
+    if (rows.length > 1) { 
+      const fileName = `${tableName}.tsv`
+      const data = rows.map(row => row.join('\t')).join('\n')
+      fs.writeFile(fileName, data, (err) => {
+        if (err)
+          console.log(err);
+        else {  
+          console.log('columns:', ...columns)
+          console.log('saved', inserts.length, `records to ${fileName}`)
+        }
+      })
+    
+    } else {
+      console.error('No records found')
+    }
+  })
+});
