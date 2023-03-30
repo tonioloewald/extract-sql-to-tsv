dc6dbfe8e2ccd9e9dfe13987ce3b5e193fcd736a
Tonio Loewald <tonio@loewald.com>
2023-03-29 20:36:23 -0700

    npm init
    [new] created package
    [new] added standardjs
    [fix] lint

--- extract.js
+++ extract.js
@@ -1,21 +1,21 @@
 // extracts tables from sql dump as TSV
 // node extract.js foo.sql
 
-const fs = require('fs');
+const fs = require('fs')
 
-function fatalError(...args) {
+function fatalError (...args) {
   console.error(...args)
   process.exit()
 }
 
-let [,, fileName] = process.argv
+const [,, fileName] = process.argv
 
-function getCells(sqlString) {
+function getCells (sqlString) {
   const cells = []
   const origString = sqlString
   // attempt to fix bizarre JSON encoding
   sqlString = sqlString.replace(/''([^':]+)'':/g, '"$1":')
-                       .replace(/: ''([^':]*)''/g, ': "$1"')
+    .replace(/: ''([^':]*)''/g, ': "$1"')
   while (sqlString) {
     if (sqlString.startsWith('\'\', ') || sqlString === '\'\'') {
       cells.push('')
@@ -24,7 +24,7 @@ function getCells(sqlString) {
       let endIndex
       try {
         endIndex = sqlString.match(/([^']|'')'(,|$)/).index
-      } catch(e) {
+      } catch (e) {
         console.error('cell termination error', e, sqlString, origString)
         process.exit()
       }
@@ -44,18 +44,16 @@ fs.readFile(fileName, 'utf8', (err, source) => {
     fatalError(err)
     return
   }
-  
-  const inserts = []
+
   const lines = source.split('\n')
-  
   const tables = {}
-  
-  for(let i = 0; i < lines.length; i++) {
+
+  for (let i = 0; i < lines.length; i++) {
     let line = lines[i]
     if (line && line.startsWith('INSERT INTO')) {
       const tableName = line.split(' ')[2]
       const record = [line]
-      while(line != null && !line.match(/(, \w+|')\);$/)) {
+      while (line != null && !line.match(/(, \w+|')\);$/)) {
         i += 1
         line = lines[i]
         record.push(line)
@@ -73,53 +71,51 @@ fs.readFile(fileName, 'utf8', (err, source) => {
       */
     }
   }
-  
+
   Object.keys(tables).forEach(tableName => {
     const table = tables[tableName]
-    const {inserts} = table
+    const { inserts } = table
     const columns = inserts[0].match(/\(([^)]+)\) VALUES/)[1].split(', ').map(x => x.startsWith('"') ? x.substring(1, x.length - 2) : x)
     const rows = [columns]
-    
+
     inserts.forEach(insert => {
       let cellData, cells
       try {
         try {
           const startIndex = insert.indexOf(' VALUES (')
-          if (startIndex > -1)
-          cellData = insert.substring(startIndex + 9, insert.length -2)
-        } catch(e) {
+          if (startIndex > -1) { cellData = insert.substring(startIndex + 9, insert.length - 2) }
+        } catch (e) {
           console.log('cellData error', e, insert)
           process.exit()
         }
         cells = getCells(cellData)
         if (cells.length !== columns.length) {
           const obj = {}
-          columns.forEach((key, idx) => obj[key] = cells[idx])
-          console.log({cellData, cells, columns, obj})
+          columns.forEach((key, idx) => {
+            obj[key] = cells[idx]
+          })
+          console.log({ cellData, cells, columns, obj })
           console.error(cells, 'has wrong number of values, found', cells.length, 'expected', columns.length)
           process.exit()
         }
         rows.push(cells)
-      } catch(e) {
+      } catch (e) {
         console.error('error:', e)
-        console.log({insert, cellData, cells})
+        console.log({ insert, cellData, cells })
         process.exit()
       }
     })
-    if (rows.length > 1) { 
+    if (rows.length > 1) {
       const fileName = `${tableName}.tsv`
       const data = rows.map(row => row.join('\t')).join('\n')
       fs.writeFile(fileName, data, (err) => {
-        if (err)
-          console.log(err);
-        else {  
+        if (err) { console.log(err) } else {
           console.log('columns:', ...columns)
           console.log('saved', inserts.length, `records to ${fileName}`)
         }
       })
-    
     } else {
       console.error('No records found')
     }
   })
-});
+})
